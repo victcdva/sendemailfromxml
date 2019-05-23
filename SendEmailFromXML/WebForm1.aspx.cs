0b8d3b7c263f2a23dafda51b4f564aff76ceb474
Víctor Córdova <theslapboy@gmail.com>
2019-05-22 17:33:11 -0700

    Send a email with Xml file

---
+++ SendEmailFromXML/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace SendEmailFromXML
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        List<student> li = new List<student>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Server.MapPath("~/student.xml"));
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/Students/student");
+            
+            foreach(XmlNode node in nodeList)
+            {
+                student s = new student();
+
+                s.id = Convert.ToInt32(node.SelectSingleNode("id").InnerText);
+                s.name = node.SelectSingleNode("name").InnerText;
+                s.age = Convert.ToInt32(node.SelectSingleNode("age").InnerText);
+                li.Add(s);
+            }
+
+            GridView1.DataSource = li;
+            GridView1.DataBind();
+        }
+    }
+}
---
+++ SendEmailFromXML/student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SendEmailFromXML
+{
+    public class student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+
+
+    }
+}
